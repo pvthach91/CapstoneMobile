8c3979e8e2de7b9ab6015828f5435ff3c62473ae
Thach <pvthach91@gmail.com>
2020-03-03 15:04:03 +0800

    update code

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { UserData } from '../../providers/user-data';
 
 import { UserOptions } from '../../interfaces/user-options';
+import {AuthService} from "../../auth/auth.service";
+import {TokenStorageService} from "../../auth/token-storage.service";
+import {AuthLoginInfo} from "../../auth/login-info";
 
 
 
@@ -13,21 +16,55 @@ import { UserOptions } from '../../interfaces/user-options';
   templateUrl: 'login.html',
   styleUrls: ['./login.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit{
   login: UserOptions = { username: '', password: '' };
   submitted = false;
 
+  form: any = {};
+  private loginInfo: AuthLoginInfo;
+
   constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
     public userData: UserData,
     public router: Router
   ) { }
+
+  ngOnInit() {
+    if (this.tokenStorage.isLoggedIn()) {
+      this.router.navigateByUrl('/app/tabs/schedule');
+    }
+  }
 
   onLogin(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
-      this.userData.login(this.login.username);
-      this.router.navigateByUrl('/app/tabs/schedule');
+      // this.userData.login(this.login.username);
+      // this.router.navigateByUrl('/app/tabs/schedule');
+
+      this.loginInfo = new AuthLoginInfo(
+        this.form.username,
+        this.form.password);
+
+      this.authService.attemptAuth(this.loginInfo).subscribe(
+        data => {
+          if (data.success == true) {
+            this.tokenStorage.saveToken(data.data.accessToken);
+            this.tokenStorage.saveUsername(data.data.username);
+            this.tokenStorage.saveFullName(data.data.fullName);
+            this.tokenStorage.saveAuthorities(data.data.authorities);
+            this.router.navigateByUrl('/app/tabs/schedule');
+          } else {
+            console.log('failed:' + data.message);
+          }
+        },
+        error => {
+          console.log('failed');
+        }
+      );
+
+
     }
   }
 
