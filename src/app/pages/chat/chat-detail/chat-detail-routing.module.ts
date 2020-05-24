ba0d8923755aea9d04f7cace94a846fd926a3eaf
Thach <pvthach91@gmail.com>
2020-05-24 21:20:34 +0800

    update code

--- src/app/pages/chat/chat-detail/chat-detail-routing.module.ts
+++ src/app/pages/chat/chat-detail/chat-detail-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ChatPage } from './chat.page';
+import { ChatDetailPage } from './chat-detail.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ChatPage
-  },
-  {
-    path: 'detail/:id',
-    loadChildren: () => import('./chat-detail/chat-detail.module').then( m => m.ChatDetailPageModule)
+    component: ChatDetailPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ChatPageRoutingModule {}
+export class ChatDetailPageRoutingModule {}
