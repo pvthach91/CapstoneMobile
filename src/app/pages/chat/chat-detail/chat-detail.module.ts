2e5b06a250141aeb886f1a0974df4c8895a641a9
Thach <pvthach91@gmail.com>
2020-05-24 22:09:23 +0800

    update code

--- src/app/pages/chat/chat-detail/chat-detail.module.ts
+++ src/app/pages/chat/chat-detail/chat-detail.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ChatDetailPageRoutingModule } from './chat-detail-routing.module';
 
 import { ChatDetailPage } from './chat-detail.page';
+import {ChatBubbleComponent} from "../chat-bubble/chat-bubble.component";
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ChatDetailPage } from './chat-detail.page';
     IonicModule,
     ChatDetailPageRoutingModule
   ],
-  declarations: [ChatDetailPage]
+  declarations: [ChatDetailPage, ChatBubbleComponent]
 })
 export class ChatDetailPageModule {}
--- src/app/pages/chat/chat-detail/chat-detail.page.ts
+++ src/app/pages/chat/chat-detail/chat-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MessageChat} from "../../../model/message.model";
 
 @Component({
   selector: 'app-chat-detail',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-detail.page.scss'],
 })
 export class ChatDetailPage implements OnInit {
+  messages:Array<MessageChat> = new Array<MessageChat>();
+  // messages:Array<any> = new Array<any>();
 
   constructor() { }
 
   ngOnInit() {
+    let m1 = new MessageChat('assets/images/owner.png', 'right', 'Hi test', 'Thach Pham', '2020/12/12');
+    let m2 = new MessageChat('assets/images/owner.png', 'left', 'Hi test rep', 'Fatima', '2020/12/12');
+    let m3 = new MessageChat('assets/images/owner.png', 'right', 'Hi test', 'Thach Pham', '2020/12/12');
+
+
+    // let m1 = {'img':'assets/images/owner.png', 'position':'right', 'content':'Hi test', 'senderName':'Thach Pham', 'time':'2020/12/12'};
+    // let m2 = {'img':'assets/images/owner.png', 'position':'left', 'content':'Hi test rep please sell me this product', 'senderName':'Fatima', 'time':'2020/12/12'};
+    // let m3 = {'img':'assets/images/owner.png', 'position':'right', 'content':'Hi test hahaha', 'senderName':'Thach Pham', 'time':'2020/12/12'};
+
+    this.messages.push(m1);
+    this.messages.push(m2);
+    this.messages.push(m3);
   }
 
 }
