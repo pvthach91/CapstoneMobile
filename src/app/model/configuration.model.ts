04192b624172f21e7cefb4ca429133b6f330539b
Thach <pvthach91@gmail.com>
2020-10-17 16:03:21 +0800

    config

--- src/app/model/configuration.model.ts
+++ src/app/model/configuration.model.ts
@@ -7,7 +7,8 @@ export class Configuration {
 
 
 export const configuration: Configuration = {
-  host: 'http://localhost:8080',
+  // host: 'http://localhost:8080',
+  host: 'http://10.0.2.2:8080',
   pageSize: 10,
   pageSizeGrid: 9,
   appName: 'THACH FARM'
